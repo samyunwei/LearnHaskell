7488d1b0eca02c09182fda795089730410b15762
sam <259375702@qq.com>
2017-02-07 00:11:01 +0800

    add module

---
+++ module.hs
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------------
+-- File Name: module.hs
+-- Author: Sam
+-- mail: samyunwei@163.com
+-- Created Time: Tue 07 Feb 2017 12:01:54 AM CST
+----------------------------------------------------------------------------
+--import Data.List
+--numUniques :: (Eq a) => [a] -> Int
+--numUniques = length . nub
+--import Data.List (nub,sort)
+--import Data.List hiding(nub)
+--import qualified Data.Map
+import qualified Data.Map as M
