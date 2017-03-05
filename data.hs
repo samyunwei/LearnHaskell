6b9270d5050ac9dd1c21d0f1e4f0cc28330faa16
samyunwei <259375702@qq.com>
2017-03-05 23:36:07 +0800

    add data

--- data.hs
+++ data.hs
@@ -75,12 +75,35 @@
 --tellCar :: Car ->  String
 --tellCar :: (Show a) => Car String String a -> String
 --tellCar (Car {company = c,model = m,year = y}) = "This " ++ c ++ " " ++ m ++ "was made in " ++ show y
-data Vector a = Vector a a a deriving (Show)
-vplus :: (Num a) => Vector a -> Vector a -> Vector a
-(Vector i j k) `vplus` (Vector l m n) = Vector (i +l) (j+m) (k+n)
+--data Vector a = Vector a a a deriving (Show)
+--vplus :: (Num a) => Vector a -> Vector a -> Vector a
+--(Vector i j k) `vplus` (Vector l m n) = Vector (i +l) (j+m) (k+n)
+--
+--dotProd :: (Num a) => Vector a -> Vector a -> a
+--(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
+--
+--vmult :: (Num a) => Vector a -> a -> Vector a
+--(Vector i j k) `vmult` m = Vector (i * m ) (j * m) (k * m)
 
-dotProd :: (Num a) => Vector a -> Vector a -> a
-(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
 
-vmult :: (Num a) => Vector a -> a -> Vector a
-(Vector i j k) `vmult` m = Vector (i * m ) (j * m) (k * m)
+data Person = Person{
+    firstName :: String
+   ,lastName  :: String
+   ,age :: Int
+}deriving (Eq)
+
+
+mikeD = Person{ firstName="Michael"
+                ,lastName = "Diamond"
+                ,age = 43
+}
+
+adRock = Person{ firstName="Adam"
+                ,lastName = "Horovitz"
+                ,age = 41
+}
+
+mca = Person{ firstName="Adam"
+                ,lastName = "Yauch"
+                ,age = 44
+}
