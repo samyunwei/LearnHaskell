8136f02fc9e8c392b33794337b244bb5b3ade897
sam <259375702@qq.com>
2017-03-08 01:03:50 +0800

    add datas

--- data.hs
+++ data.hs
@@ -128,25 +128,73 @@
 --type AssocList k v = [(k,v)]
 --type InMap = Map Int
 --
-import qualified Data.Map as Map
+--import qualified Data.Map as Map
+--
+--data LockerState = Taken | Free deriving (Show,Eq)
+--type Code = String
+--type LockerMap = Map.Map Int (LockerState,Code)
+--
+--lockerLookup :: Int -> LockerMap -> Either String Code
+--lockerLookup lockerNumber map = case Map.lookup lockerNumber map of 
+--    Nothing -> Left $ "Locker" ++ show lockerNumber ++ "doesn't exist!"
+--    Just (state,code) -> if state /= Taken
+--                            then Right code
+--                            else Left $ "Locker" ++ show lockerNumber ++ "is alreadytaken"
+--
+--lockers :: LockerMap
+--lockers = Map.fromList[(100,(Taken,"ZD39I"))
+--                        ,(101,(Free,"JAH3I"))
+--                        ,(103,(Free,"IQSA9"))
+--                        ,(105,(Free,"QOTSA"))
+--                        ,(109,(Taken,"893JJ"))
+--                        ,(110,(Taken,"99292"))
+--                        ]
+--
 
-data LockerState = Taken | Free deriving (Show,Eq)
-type Code = String
-type LockerMap = Map.Map Int (LockerState,Code)
+--
+--data List a = Empty | Cons {
+--    listHead :: a
+--    ,listTail :: List a} 
+--   deriving (Show,Read,Eq,Ord)
+--
+--
+--infixr 5 :-:
+--data List a = Empty | a :-: (List a) deriving (Show,Read,Eq,Ord)
+--
+--
+--infixr 5 ^++
+--(^++) :: List a -> List a -> List a
+--Empty ^++ ys = ys
+--(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+--
+--data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+--
+--singleton :: a -> Tree a
+--singleton x = Node x EmptyTree EmptyTree
+--
+--treeInsert :: (Ord a) => a -> Tree a -> Tree a
+--treeInsert x EmptyTree = singleton x
+--treeInsert x (Node a left right)
+--    | x == a = Node x left right
+--    | x < a = Node a (treeInsert x left) right
+--    | x >a = Node a left (treeInsert x right)
+--
+--treeElem :: (Ord a) => a -> Tree a -> Bool
+--treeElem x EmptyTree = False
+--treeElem x (Node a left right)
+--    | x == a =True
+--    | x < a = treeElem x left
+--    | x > a = treeElem x right
 
-lockerLookup :: Int -> LockerMap -> Either String Code
-lockerLookup lockerNumber map = case Map.lookup lockerNumber map of 
-    Nothing -> Left $ "Locker" ++ show lockerNumber ++ "doesn't exist!"
-    Just (state,code) -> if state /= Taken
-                            then Right code
-                            else Left $ "Locker" ++ show lockerNumber ++ "is alreadytaken"
 
-lockers :: LockerMap
-lockers = Map.fromList[(100,(Taken,"ZD39I"))
-                        ,(101,(Free,"JAH3I"))
-                        ,(103,(Free,"IQSA9"))
-                        ,(105,(Free,"QOTSA"))
-                        ,(109,(Taken,"893JJ"))
-                        ,(110,(Taken,"99292"))
-                        ]
+data TrafficLight = Red | Yellow | Green
+instance Eq TrafficLight where
+        Red == Red = True
+        Green == Green = True
+        Yellow == Yellow = True
+        _ == _ = False
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
 
